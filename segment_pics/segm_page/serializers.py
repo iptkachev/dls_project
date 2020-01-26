11c49a091a30c85736d54ab11640d6d63b32b6aa
Ilya Tkachev <skaatt@mail.ru>
2020-01-26 21:38:45 +0300

    delete serializer

--- segment_pics/segm_page/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import PictureFile
-
-
-class PictureSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PictureFile
-        fields = '__all__'

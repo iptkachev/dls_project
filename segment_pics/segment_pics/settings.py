046823947635030e4b2cb1a776a08e596dce89b1
Ilya Tkachev <skaatt@mail.ru>
2020-02-01 22:19:23 +0300

    added url

--- segment_pics/segment_pics/settings.py
+++ segment_pics/segment_pics/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'pl^12t2opqvm!snf%k=m$5@sjij348oa&ha5otwp_rq078pm4j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
